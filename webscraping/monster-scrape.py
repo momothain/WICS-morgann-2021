5baca711cec36fb9531f15deb55cffe2250ae32c
Morgann Thain <morgann.thain@gmail.com>
2021-06-04 17:39:47 -0400

    mostly worked through monster example with id, class_ find, but from the start some internally nested names/tags/data seems to be messed up/scrambled. could not try find_all, but would results-card inside results-list

--- webscraping/monster-scrape.py
+++ webscraping/monster-scrape.py
@@ -7,18 +7,31 @@ URL = 'https://www.monster.com/jobs/search?q=Software+Developer&where=Pennsylvan
 page_GET_request_response = requests.get(URL)
 pcontent = page_GET_request_response.content
 
-pp = pprint.PrettyPrinter(indent=2)
-#pp.pprint(pcontent)
+pp = pprint.PrettyPrinter(indent=2) #pp.pprint(pcontent)
+
 
 ## use BeautifulSoup to parse html/dom
 # can search html elements by id, classname
-soup = BeautifulSoup(pcontent, 'html.parser')
-#print(soup.prettify())
+soup = BeautifulSoup(pcontent, 'html.parser') #print(soup.prettify())
+
 # id
 app = soup.find(id='app')
-print(app.prettify()) #prints better
-#results = soup.find(id='results-page container')
-#print(results.prettify()) #prints better
+# print(app.prettify()) #prints better #results = soup.find(id='results-page container') #print(results.prettify()) #prints better
+
 # class name
+lay = app.find(class_='layout-container') #'section' first arg breaks this
+layquick = soup.find(class_='layout-container') # gives the same as above
+print(lay.prettify())
+
+res = lay.find(class_='results-page container'
+            )# for some reason the names are scrambled after this point...
+            #.find(class_='row isMobileRange')
+print(res.prettify())
+
+res_list = res.find_all('results-card')
+## below prints nothing
+# layout = app.find_all('section', class_='layout-container')
+# for e in layout:
+    # print(e.prettify, end='\n'*2)
 
 # job_elems = results.find_all('section', class_='card-content')
