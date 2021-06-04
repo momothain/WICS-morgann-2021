1a66de12cd85e7b2bb84c0c2f575c49f9a31402c
Morgann Thain <morgann.thain@gmail.com>
2021-06-04 16:55:50 -0400

    basic workflow with soup established

--- webscraping/monster-scrape.py
+++ webscraping/monster-scrape.py
@@ -1,9 +1,24 @@
 import requests
 import pprint
+from bs4 import BeautifulSoup
 
-URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
-page= requests.get(URL)
-printable_page = page.content
+## use requests to send a get request and get the html content
+URL = 'https://www.monster.com/jobs/search?q=Software+Developer&where=Pennsylvania'
+page_GET_request_response = requests.get(URL)
+pcontent = page_GET_request_response.content
 
 pp = pprint.PrettyPrinter(indent=2)
-#pp.pprint(printable_page)
+#pp.pprint(pcontent)
+
+## use BeautifulSoup to parse html/dom
+# can search html elements by id, classname
+soup = BeautifulSoup(pcontent, 'html.parser')
+#print(soup.prettify())
+# id
+app = soup.find(id='app')
+print(app.prettify()) #prints better
+#results = soup.find(id='results-page container')
+#print(results.prettify()) #prints better
+# class name
+
+# job_elems = results.find_all('section', class_='card-content')
